a209932075fe948e47a8e301624e30e54cb95e1d
Mike Donnici <michael@mesa.net.au>
2018-02-23 14:31:14 +1100

    Update

--- pages/membership/index.vue
+++ pages/membership/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <h1>Membership</h1>
+  <div>
+    <PageHeader
+      title="Membership"
+      subtitle="The benefits of CSANZ membership"
+    />
+    <section class="section">
+      <div class="container">
+        <div class="content">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores blanditiis consectetur distinctio
+            dolores dolorum eius, facilis fugiat iusto, nostrum recusandae sapiente veniam? Atque expedita explicabo
+            minus
+            officiis provident sapiente sed!</p>
+          <p>Animi asperiores atque explicabo labore, reiciendis rem repellat. Aperiam cum deleniti enim est explicabo
+            impedit iste iure labore laborum numquam officia optio perspiciatis, quibusdam quisquam quos, reiciendis
+            sint
+            ut voluptatibus?</p>
+          <p>Aliquam aliquid asperiores nihil obcaecati repellat? Ad animi debitis dicta fugit in laudantium magni natus
+            ratione similique, vero? Amet autem consequatur, consequuntur dolorem earum esse natus nulla quis
+            repellendus
+            veniam.</p>
+          <p>Eos iste, officiis. Assumenda commodi consequatur dolorum eius eos explicabo facere labore modi, molestias
+            officiis optio perferendis provident saepe sint tempore vel veniam vero vitae. Eius incidunt labore odio
+            totam?</p>
+          <p>Ab adipisci aliquid at consequuntur error explicabo illo, in iure libero modi nam numquam optio pariatur
+            porro praesentium quas, quasi reiciendis saepe sapiente veritatis. Blanditiis dicta dolorum labore nemo
+            quam.</p>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "index"
-    }
+  export default {
+    name: "index"
+  }
 </script>
 
 <style scoped>
